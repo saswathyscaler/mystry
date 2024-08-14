f0eef4823bace8977651dea4afad1b1806f25fb7
Saswat Ranjan <saswatr.mohanty@hyscaler.com>
2024-08-14 17:37:49 +0530

    .

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,11 @@
+import {z} from 'zod'
+
+export const usernameValidation = z
+  .string()
+  .min(2, "Username must be at least 2 characters")
+  .max(20, "Username must be no more than 20 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters");
+
+export const signUpSchema = z.object({
+  // Define other schema fields here
+});
